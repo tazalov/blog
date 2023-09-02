9587c05f963da5689b1658c52b83fbd36ec5712a
tazalov <tazalov9@gmail.com>
2023-09-02 15:53:03 +0300

    upd: webpack loaders (svg, files),

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,35 +4,51 @@ import {BuildOptions} from './types/config';
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
   /*
-   Дорогой читатель, здесь мы собираем loaders
-   Вызываем функцию в buildWebPackConfig в ключе module.rules
+   * Дорогой читатель, здесь мы собираем loaders
+   * Вызываем функцию в buildWebPackConfig в ключе module.rules
    */
   
-  //! Порядок loaders в массиве важен!
-  //Если не используем TS, нужен babel-loader
+  const svgLoader = {
+    test: /\.svg$/i,
+    issuer: /\.[jt]sx?$/,
+    use: ['@svgr/webpack'],
+  };
+  
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  };
+  
+  //* Если не используем TS, нужен babel-loader
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
     exclude: /node_modules/,
   };
+  
   //! Лоадер настроен только на scss и sass, CSS файлы не будут работать
   const stylesLoaders = {
     test: /\.s[ac]ss$/i,
     use: [
       //! MiniCssExtractPlugin нужен для того, чтобы наши стили попадали в отдельный css файл, а не оставались в js файле
-      //нужен только в production
+      //* нужен только в production
       options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
       // Translates CSS into CommonJS
       {
         loader: 'css-loader',
         options: {
           modules: {
-            //Генерируем сложные названия классов только для файлов модульных стилей
-            auto: ((resourcePath: string) => Boolean(resourcePath.includes('.module'))),
-            //Включаем эту генерацию только в production, в development
+            //* Генерируем сложные названия классов только для файлов модульных стилей
+            auto: ((resourcePath: string) => Boolean(
+                resourcePath.includes('.module'))),
+            //* Включаем эту генерацию только в production, в development
             localIdentName: options.isDev
                 ? '[path][name]__[local]--[hash:base64:5]'
-                : '[hash:base64:8]'
+                : '[hash:base64:8]',
           },
         },
       },
@@ -41,5 +57,6 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     ],
   };
   
-  return [typescriptLoader, stylesLoaders];
+  //! Порядок loaders в массиве важен!
+  return [svgLoader, fileLoader, typescriptLoader, stylesLoaders];
 }
