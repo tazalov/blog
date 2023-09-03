aa35f74c541d1220e5500f71346c31d8abaaa16e
tazalov <tazalov9@gmail.com>
2023-09-03 10:07:09 +0300

    config: webpack hot-module-replacement + react-refresh

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,22 +1,27 @@
+import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 import {BuildOptions} from './types/config';
 
-export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins({
+  paths,
+  isDev,
+}: BuildOptions): webpack.WebpackPluginInstance[] {
   /*
    * Дорогой читатель, здесь мы собираем основные плагины, которые нужны для разработки
    * Вызываем функцию в buildWebPackConfig в ключе plugins
    */
   
   /*
-  * ProgressPlugin - показывать процент прогресса при билде
-  * HtmlWebpackPlugin - нужен, чтобы вебпак в билдовый index.html подключил наш бандл
-  * DefinePlugin - прокидываем переменные или данные из вебпака в наш проект, здесь нам нужен isDev для i18next
-  * */
+   * ProgressPlugin - показывать процент прогресса при билде
+   * HtmlWebpackPlugin - нужен, чтобы вебпак в билдовый index.html подключил наш бандл
+   * DefinePlugin - прокидываем переменные или данные из вебпака в наш проект, здесь нам нужен isDev для i18next
+   * HotModuleReplacementPlugin - позволяет менять данные в приложении или стили без перезагрузки проекта (страницы)
+   * ReactRefreshPlugin  - предыдущий плагин плохо работает с реакт-компонентами, поэтому в довесок ставим и эту хрень
+   * */
   
-  
-  return [
+  const plugins = [
     new webpack.ProgressPlugin(),
     new HtmlWebpackPlugin(
         {template: paths.html}),
@@ -25,8 +30,14 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
       chunkFilename: 'css/[name].[contenthash:8].css',
     }),
     new webpack.DefinePlugin({
-        __IS_DEV__: JSON.stringify(isDev)
-      })
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
   ];
   
+  if (isDev) {
+    plugins.push(new ReactRefreshPlugin(),
+        new webpack.HotModuleReplacementPlugin());
+  }
+  
+  return plugins;
 }
